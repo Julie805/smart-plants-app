fe3d6865415f37aefa9225db96a399875d5b7b3c
Julie M <seejulielee@gmail.com>
2023-03-10 20:34:52 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>The Amazing Smart Plants App</h1>
+    </div>
+  )
+}
+
+export default App
