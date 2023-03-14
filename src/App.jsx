7719cf1e6e7ca3e02791c41366b38a1ca9ffed9a
Julie M <seejulielee@gmail.com>
2023-03-14 14:58:55 -0700

    Updated App content to include base table setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
 import './App.css'
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+// import "./styles.css";
+// import BasicTable from "./Table";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function App() {
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(",")
+  },
+  palette: {
+    primary: {
+      main: "#0A193F"
+    },
+    secondary: {
+      main: "#74AEFA"
+    }
+  }
+});
 
+export default function App() {
 
   return (
-    <div className="App">
-      <h1>The Amazing Smart Plants App</h1>
-    </div>
+    <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Typography variant="h5" sx={{ fontWeight: "800", p: 2.5 }}>
+          {" "}
+          Best Smart Plants App Ever
+        </Typography>
+        {/* <BasicTable></BasicTable> */}
+      </div>
+    </ThemeProvider>
+  </React.Fragment>
   )
 }
 
-export default App
